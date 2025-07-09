f9a1f22bbbe2f005422c8dd69af10432173244a0
Nicolas Morales <30967611+nicomts@users.noreply.github.com>
2025-07-09 18:41:41 -0300

    feat(models): add enums for Difficulty and Category

---
+++ TriviaSharp/Models/Enums/Category.cs
@@ -0,0 +1,11 @@
+namespace TriviaSharp.Models.Enums;
+
+public enum Category
+{
+    General,
+    Science,
+    History,
+    Arts,
+    Sports,
+    Entertainment
+}
---
+++ TriviaSharp/Models/Enums/Difficulty.cs
@@ -0,0 +1,8 @@
+namespace TriviaSharp.Models.Enums;
+
+public enum Difficulty
+{
+    Low,
+    Medium,
+    High
+}
