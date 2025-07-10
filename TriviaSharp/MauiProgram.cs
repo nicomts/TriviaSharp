6eaf270b449437b82839db9c50c843a9be7294d3
Nicolas Morales <30967611+nicomts@users.noreply.github.com>
2025-07-10 12:43:56 -0300

    chore: configure services and dependency injection

--- TriviaSharp/MauiProgram.cs
+++ TriviaSharp/MauiProgram.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using TriviaSharp.Data;
+using TriviaSharp.Data.Repositories;
 
 namespace TriviaSharp;
 
@@ -14,6 +17,18 @@ public static class MauiProgram
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
             });
+        // Configure SQLite with EF Core
+        builder.Services.AddDbContext<TriviaDbContext>(options =>
+            options.UseSqlite("Filename=triviasharp.db")); // You can change the path if needed
+
+        // Register generic and specific repositories
+        builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+        builder.Services.AddScoped<IQuestionRepository, QuestionRepository>();
+        builder.Services.AddScoped<IAnswerRepository, AnswerRepository>();
+        builder.Services.AddScoped<IUserRepository, UserRepository>();
+        builder.Services.AddScoped<IQuizSessionRepository, QuizSessionRepository>();
+        builder.Services.AddScoped<IQuestionSetRepository, QuestionSetRepository>();
+
 
 #if DEBUG
         builder.Logging.AddDebug();
